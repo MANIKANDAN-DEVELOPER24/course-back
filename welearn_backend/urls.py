8ca4797f14601980d245662e62549e6455e8e468
Manikandan <manikandan.work.it@gmail.com>
2025-08-14 17:05:08 +0530

    setting

--- welearn_backend/urls.py
+++ welearn_backend/urls.py
@@ -1,44 +1,3 @@
-
-# # welearn_backend/urls.py
-# from django.contrib import admin
-# from django.urls import path
-# from core import views
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-
-#     # auth
-#     path('api/register/', views.RegisterView.as_view()),
-#     path('api/login/', views.login_view),
-#     path('api/current-user/', views.current_user),
-
-#     # users
-#     path('api/users/', views.UserListView.as_view()),
-#     path('api/users/<int:pk>/', views.UserDetailView.as_view()),
-
-#     # courses
-#     path('api/courses/', views.CourseListCreateView.as_view()),
-#     path('api/all-courses/', views.CourseListView.as_view()),
-
-#   # Offers endpoints
-#     path('api/offers/', views.OfferListCreateView.as_view(), name='offer-list'),
-#     path('api/offers/<int:pk>/', views.OfferRetrieveUpdateDeleteView.as_view(), name='offer-detail'),
-
-
-#  # Purchases / Checkout
-#     path('api/purchases/', views.PurchaseListView.as_view()),
-#     path('api/checkout/', views.checkout, name='checkout'),
-#      path('login/', views.login_view),
-#     path('checkout/', views.checkout),
-
-# ]
-
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -56,8 +15,8 @@ urlpatterns = [
 
     # JWT auth
     path('api/login/', TokenObtainPairView.as_view(), name='login'),  # Alias for React login
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Standard JWT login
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh access token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Registration & current user
     path('api/register/', views.RegisterView.as_view(), name='register'),
@@ -80,5 +39,5 @@ urlpatterns = [
     path('api/checkout/', views.checkout, name='checkout'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ✅ Always serve media in local AND production (for Render)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
