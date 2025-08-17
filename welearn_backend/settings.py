9e481d1a7f84aca692b4461c18905842b47411a9
Manikandan <manikandan.work.it@gmail.com>
2025-08-17 13:46:50 +0530

    key

--- welearn_backend/settings.py
+++ welearn_backend/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'cloudinary_storage',
 ]
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': 'dmh2ni2jh',
+    'API_KEY':'183391876677299',
+    'API_SECRET': 'sJweMTTCiWvh2ZkaWcUKUirre3w',
 }
 
 MIDDLEWARE = [
