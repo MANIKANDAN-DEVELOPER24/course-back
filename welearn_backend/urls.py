03d58f977c27fcd6ee2fb69a7df1c8d4fd9a78af
Manikandan <manikandan.work.it@gmail.com>
2025-08-18 14:08:50 +0530

    resolve

--- welearn_backend/urls.py
+++ welearn_backend/urls.py
@@ -107,6 +107,7 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
+
 # from django.contrib import admin
 # from django.urls import path
 # from django.conf import settings
